5fd637ba8c1dffa5473ca5c1a6d929b10e4f2555
ideal-git-master <git@idealdirections.co.jp>
2026-01-22 22:29:48 +0900

    Add component css files

--- parts/head.php
+++ parts/head.php
@@ -5,16 +5,27 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
-  <!-- CSS -->
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+
+  <!-- External Libraries CSS -->
   <link rel="stylesheet" href="../assets/vendor/swiper11.1.15/swiper-bundle.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+
+  <!-- Common CSS -->
+  <link rel="stylesheet" href="../assets/css/header.css">
+  <link rel="stylesheet" href="../assets/css/footer.css">
   <link rel="stylesheet" href="../assets/css/common.css">
+
+  <!-- Component CSS -->
+  <link rel="stylesheet" href="../assets/css/medical-program-search.css">
+  <link rel="stylesheet" href="../assets/css/medical-program-keyword-search.css">
+  <link rel="stylesheet" href="../assets/css/ai-link-banner.css">
+
   <!-- JS -->
   <script src="../assets/vendor/swiper11.1.15/swiper-bundle.min.js" defer></script>
-  <script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js" defer></script>
   <script src="https://cdn.tailwindcss.com" defer></script>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" defer></script>
   <script src="../assets/js/common.js" defer></script>
